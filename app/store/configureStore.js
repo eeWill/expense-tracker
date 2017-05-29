f89d0de13ac9a2dc181f9e6fbdc3cc0375a01326
eeWill <eewill.github@gmail.com>
2017-05-29 17:14:01 -0400

    Notication is on timeout and style improved

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -3,12 +3,15 @@ import createLogger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import { mySaga } from '../sagas/expenses.js';
 import AppReducer from '../reducers';
+import { reduxTimeout } from 'redux-timeout'
 
 const middleware = [];
 
 if (process.env.NODE_ENV === 'development') {
   middleware.push(createLogger());
 }
+
+middleware.push(reduxTimeout());
 
 let initialState = {
   expenses: [],
